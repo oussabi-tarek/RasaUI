0121e30327030610b5e4e5c4a9748e9af66ea447
Tarek Oussabi <oussabi.tarek@gmail.com>
2022-12-23 02:05:56 +0100

    first commit

---
+++ rasa/readme.md
@@ -0,0 +1,11 @@
+# open rasa folder
+
+open 2 commands prompt
+in the first run:
+rasa run actions # for starting server
+the second run:
+rasa run --cors "\*" --enable-api # for enabling api
+
+# open react floder
+
+npm start
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Basic from "./components/Basic";
+
+function App() {
+  return (
+    <div className="App">
+      <h1></h1>
+      <h5>
+        <img></img>
+      </h5>
+      <Basic />
+    </div>
+  );
+}
+
+export default App;
